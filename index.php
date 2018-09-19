5d66e2b21fb7c3aea56bf3b2e9e631bde805f0b1
Dreft12 <jorgeelieceraldana.92@gmail.com>
2018-09-19 12:56:50 -0500

    V 0.1.2 Agregando Vuetify
    Se añade la libreria Vuetify para los componentes de visualización.

--- index.php
+++ index.php
@@ -4,5 +4,33 @@ session_start();
 if(!isset($_SESSION['Controller'])){
     $_SESSION['Controller'] = new Controller();
 }
+?>
 
-$_SESSION['Controller']->create();
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+    <link href="app/css/vuetify.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+</head>
+<body>
+<div id="app">
+    <v-app>
+        <v-navigation-drawer app></v-navigation-drawer>
+        <v-toolbar app>ColsamApp</v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+        <v-footer app></v-footer>
+    </v-app>
+</div>
+
+<script src="app/javascript/vue.js"></script>
+<script src="app/javascript/vuetify.js"></script>
+<script>
+    new Vue({ el: '#app' })
+</script>
+</body>
+</html>

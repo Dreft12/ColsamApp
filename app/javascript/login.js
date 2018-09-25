1794a9e2521dceb5f826a5d03c9ece999d8da094
Dreft12 <jorgeelieceraldana.92@gmail.com>
2018-09-24 22:39:10 -0500

    V 0.2.6.1 Reparando diseño Log Out
    De ahora en adelante se guardarán las configuraciones de cada usuario en el localStorage de los navegadores
    
    Se eliminaron las funciones para obtener y llamar cookies
    
    Se reparó error al salir de sesión que borraba la configuración del tema Dark
    
    Se agregó el color blanco para el mensaje de cierre de sesion

--- app/javascript/login.js
+++ app/javascript/login.js
@@ -17,7 +17,7 @@ new Vue({
         inputDisable: false
     },
     mounted() {
-        this.isDark = (this.readCookie("dark")==='true');
+        this.isDark = (localStorage.getItem("dark")==='true');
     },
     methods: {
         validar: function () {
@@ -71,19 +71,7 @@ new Vue({
             this.pass = '';
         },
         cookieTheme: function () {
-            document.cookie = "dark=" + this.isDark;
-        },
-        readCookie: function (name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) === 0) {
-                    return decodeURIComponent(c.substring(nameEQ.length, c.length));
-                }
-            }
-            return null;
-        },
+            localStorage.setItem("dark", ''+this.isDark);
+        }
     }
 });
--- app/javascript/panelEs.js
+++ app/javascript/panelEs.js
@@ -7,27 +7,15 @@ new Vue({
     },
     mounted() {
         this.usuario = JSON.parse(localStorage.getItem('User'));
-        this.isDark = (this.readCookie("dark")==='true');
+        this.isDark = (localStorage.getItem("dark")==='true');
     },
     methods: {
         cookieTheme: function () {
-            document.cookie = "dark=" + this.isDark;
-        },
-        readCookie: function (name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) === 0) {
-                    return decodeURIComponent(c.substring(nameEQ.length, c.length));
-                }
-            }
-            return null;
+            localStorage.setItem("dark", ''+this.isDark);
         },
         cerrarSesion: function () {
             this.usuario = [];
-            localStorage.clear();
+            localStorage.removeItem('User');
             this.dialogLogout = true
             setTimeout("location.href = '/ColsamApp/index.html'", 5000);
         }

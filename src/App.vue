3ef0fc24e96288ef0550355a11cfa857304c37b9
Dreft12 <jorgeelieceraldana.92@gmail.com>
2019-01-25 23:37:32 -0500

    V 0.3.2 Añadiendo Inicio de Sesión
    Los correos se utilizarán como los ID de documentos en la colección de Firestore.
    
    Al ingresar Firestore devuelve el nombre y apellido de la persona que ingresa para mostrarle su cuenta.
    
    Se añadieron dos wallpapers de fondo para el cambio de enfasis tanto de light y dark para que el fondo sea más llamativo
    
    Se ajustaron algunos estilos
    
    Se cambió la ubicación de inicio de Firebase App

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,129 @@
 <template>
-  <v-app :dark="isDark" id="inspire">
-   <v-toolbar app color="primary" class="toolbar">
-                <v-toolbar-title class="white--text titulo">
-                    <v-img :src="require('@/assets/logo.png')" class="logo" height="66" width="54"></v-img>Bienvenidos a ColsamApp</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <v-btn flat class="white--text">Quienes somos</v-btn>
-                <v-btn flat class="white--text">Ayuda</v-btn>
-                <v-btn flat slot="activator" @click="dialogActivar" class="white--text">Contactenos</v-btn>
-                <v-dialog v-model="dialog" persistent max-width="500px">
-                    <v-card>
-                        <v-card-title>
-                            <span class="headline">Formulario de contacto</span>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-container grid-list-md>
-                                <v-layout wrap>
-                                    <v-flex xs12>
-                                        <v-text-field label="Nombres*" required></v-text-field>
-                                        <v-text-field
-                                                label="Apellidos*" required
-                                        ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12>
-                                        <v-text-field label="Correo*" required></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6>
-                                        <v-select
-                                                :items="['0-17', '18-29', '30-54', '54+']"
-                                                label="Edad*"
-                                                required
-                                        ></v-select>
-                                    </v-flex>
-                                </v-layout>
-                            </v-container>
-                            <small>*Todos los campos deben ser requeridos</small>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" flat @click.native="dialog = false">Cerrar</v-btn>
-                            <v-btn color="blue darken-1" flat @click.native="dialog = false">Enviar</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-                <v-dialog v-model="dialogMora" max-width="500px">
-                    <v-card>
-                        <v-card-title>
-                            <span class="headline">No se ha podido ingresar al sistema</span>
-                        </v-card-title>
-                        <v-card-text>
-                                <v-layout wrap>
-                                    <v-flex xs12>
-                                        No ha realizado el(los) pago(pagos) pendientes de la pensión, por favor realice los pagos pertinentes para tener acceso al panel de notas; <br>Meses en mora: {{meses}}.
-                                    </v-flex>
-                                </v-layout>
-                        </v-card-text>
-                    </v-card>
-                </v-dialog>
-                <v-btn flat icon color="white" @click="irA('https://github.com/Dreft12/ColsamApp')">
-                    <v-icon>favorite</v-icon>
-                </v-btn>
-                <v-menu bottom offset-y left>
-                    <v-btn slot="activator" dark icon>
-                        <v-icon>more_vert</v-icon>
-                    </v-btn>
-                    <v-list>
-                        <v-list-tile v-on:click="cookieTheme">
-                            <v-list-tile-title><v-switch
-                                        label="Tema oscuro"
-                                        v-model="isDark"
-                                 ></v-switch>
-                            </v-list-tile-title>
-                        </v-list-tile>
-                        <v-btn color="primary">¿Olvidaste la contraseña?</v-btn>
-                    </v-list>
-                    </v-menu>
-            </v-toolbar-items>
-        </v-toolbar>
-        <v-content>
-            <v-container fluid fill-height>
-                <v-layout align-center justify-center>
-                    <v-flex xs12 sm8 md4>
-                        <v-card class="elevation-12">
-                            <v-toolbar dark color="primary">
-                                <v-toolbar-title>Iniciar sesión</v-toolbar-title>
-                            </v-toolbar>
-                            <v-card-text>
-                                <v-alert type="error" v-model="errorPass" dismissible>Por favor ingrese una contraseña valida.</v-alert>
-                                <v-alert type="error" v-model="errorUser" dismissible>Por favor ingrese un usuario valido.</v-alert>
-                                <v-alert type="error" v-model="errorLogin" dismissible>Usuario y/o contraseña incorrecta.</v-alert>
-                                <v-alert type="error" v-model="errorDB" dismissible>Error en la comunicación hacia la base de datos.</v-alert>
-                                <v-alert type="info" v-model="redireccion">Redirigiendo al panel, por favor espere...</v-alert>
-                                <v-alert type="success" v-model="exito" dismissible v-for="us in usuario">Bienvenido al sistema: {{ us.Nombre }} {{ us.Apellido }}.</v-alert>
-                                <v-form>
-                                    <v-text-field v-model="user" :disable="inputDisable" v-on:keypress.enter="validar" name="user" prepend-icon="person" label="Usuario" type="text"></v-text-field>
-                                    <v-text-field v-model="pass" v-on:keypress.enter="validar" id="password" prepend-icon="lock"  label="Contraseña" type="password"></v-text-field>
-                                </v-form>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn v-on:click="registro" color="primary">Entrar</v-btn>
-                                <v-btn v-on:click="reset" color="primary">Limpiar</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-content>
-        <v-footer app class="footer-bar">
-            <v-container class="text-lg-center">Todos los derechos reservados 2018, Casa San Luis Beltran de Barrranquilla</v-container>
-        </v-footer>
+  <v-app :dark="isDark" id="inspire" class="back">
+      <v-toolbar app color="primary" class="toolbar">
+          <v-toolbar-title class="white--text titulo">
+              <v-img :src="require('@/assets/logo.png')" class="logo" height="66" width="54"></v-img>Bienvenidos a ColsamApp</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+              <v-btn flat class="white--text">Quienes somos</v-btn>
+              <v-btn flat class="white--text">Ayuda</v-btn>
+              <v-btn flat slot="activator" @click="dialogActivar" class="white--text">Contactenos</v-btn>
+              <v-dialog v-model="dialog" persistent max-width="500px">
+                  <v-card>
+                      <v-card-title>
+                          <span class="headline">Formulario de contacto</span>
+                      </v-card-title>
+                      <v-card-text>
+                          <v-container grid-list-md>
+                              <v-layout wrap>
+                                  <v-flex xs12>
+                                      <v-text-field label="Nombres*" required></v-text-field>
+                                      <v-text-field
+                                              label="Apellidos*" required
+                                      ></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12>
+                                      <v-text-field label="Correo*" required></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm6>
+                                      <v-select
+                                              :items="['0-17', '18-29', '30-54', '54+']"
+                                              label="Edad*"
+                                              required
+                                      ></v-select>
+                                  </v-flex>
+                              </v-layout>
+                          </v-container>
+                          <small>*Todos los campos deben ser requeridos</small>
+                      </v-card-text>
+                      <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="blue darken-1" flat @click.native="dialog = false">Cerrar</v-btn>
+                          <v-btn color="blue darken-1" flat @click.native="dialog = false">Enviar</v-btn>
+                      </v-card-actions>
+                  </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogMora" max-width="500px">
+                  <v-card>
+                      <v-card-title>
+                          <span class="headline">No se ha podido ingresar al sistema</span>
+                      </v-card-title>
+                      <v-card-text>
+                          <v-layout wrap>
+                              <v-flex xs12>
+                                  No ha realizado el(los) pago(pagos) pendientes de la pensión, por favor realice los pagos pertinentes para tener acceso al panel de notas; <br>Meses en mora: {{meses}}.
+                              </v-flex>
+                          </v-layout>
+                      </v-card-text>
+                  </v-card>
+              </v-dialog>
+              <v-btn flat icon color="white" v-on:click="irA('https://github.com/Dreft12/ColsamApp')">
+                  <v-icon>favorite</v-icon>
+              </v-btn>
+              <v-menu bottom offset-y left>
+                  <v-btn slot="activator" dark icon>
+                      <v-icon>more_vert</v-icon>
+                  </v-btn>
+                  <v-list>
+                      <v-list-tile v-on:click="cookieTheme">
+                          <v-list-tile-title><v-switch
+                                  label="Tema oscuro"
+                                  v-model="isDark"
+                          ></v-switch>
+                          </v-list-tile-title>
+                      </v-list-tile>
+                      <v-btn color="primary">¿Olvidaste la contraseña?</v-btn>
+                  </v-list>
+              </v-menu>
+          </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+          <v-container fluid fill-height>
+              <v-layout align-center justify-center>
+                  <v-flex xs12 sm8 md4>
+                      <v-card class="elevation-12">
+                          <v-toolbar dark color="primary">
+                              <v-toolbar-title>Iniciar sesión</v-toolbar-title>
+                          </v-toolbar>
+                          <v-card-text>
+                              <v-alert type="error" v-model="errorPass" dismissible>Por favor ingrese una contraseña valida.</v-alert>
+                              <v-alert type="error" v-model="errorUser" dismissible>Por favor ingrese un usuario valido.</v-alert>
+                              <v-alert type="error" v-model="errorLogin" dismissible>Usuario y/o contraseña incorrecta.</v-alert>
+                              <v-alert type="error" v-model="errorDB" dismissible>Error en la comunicación hacia la base de datos.</v-alert>
+                              <v-alert type="info" v-model="redireccion">Redirigiendo al panel, por favor espere...</v-alert>
+                              <v-alert type="success" v-model="exito" dismissible>Bienvenido al sistema: {{ nombres }}.</v-alert>
+                              <v-form>
+                                  <v-text-field v-model="user" :disable="inputDisable" v-on:keypress.enter="validar" name="user" prepend-icon="person" label="Usuario" type="text"></v-text-field>
+                                  <v-text-field v-model="pass" v-on:keypress.enter="validar" id="password" prepend-icon="lock"  label="Contraseña" type="password"></v-text-field>
+                              </v-form>
+                          </v-card-text>
+                          <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn v-on:click="login" color="primary">Entrar</v-btn>
+                              <v-btn v-on:click="reset" color="primary">Limpiar</v-btn>
+                          </v-card-actions>
+                      </v-card>
+                  </v-flex>
+              </v-layout>
+          </v-container>
+      </v-content>
+      <v-footer app class="footer-bar">
+          <v-container class="text-lg-center">Todos los derechos reservados 2018, Casa San Luis Beltran de Barrranquilla</v-container>
+      </v-footer>
   </v-app>
 </template>
 <script>
     import firebase from 'firebase'
+    const config = {
+        apiKey: "AIzaSyD8twKowSi1OFExb8i3oiwItWrXD3zZKKY",
+        authDomain: "colsamapp.firebaseapp.com",
+        databaseURL: "https://colsamapp.firebaseio.com",
+        projectId: "colsamapp",
+        storageBucket: "colsamapp.appspot.com",
+        messagingSenderId: "374315306977"
+    };
+    let app = firebase.initializeApp(config);
+    let db = firebase.firestore(app);
   export default {
     data: () => ({
       drawer: null,
@@ -130,6 +140,7 @@
         isDark: false,
         dialog: false,
         dialogMora: false,
+        nombres:'',
         inputDisable: false,
       title:'Inicio de sesión'
     }),
@@ -140,6 +151,28 @@
       source: String
     },
       methods:{
+        login: function(){
+
+         let f = firebase.auth().signInWithEmailAndPassword(this.user, this.pass).then(
+              function (){
+                    return true;
+              },
+              function () {
+                  return false
+              }
+          );
+         f.then(res => {
+             let docRef = db.collection("usuarios").doc(this.user);
+              if (res) {
+                  this.exito = true;
+                  docRef.get().then(doc =>{
+                      this.nombres = doc.data()['nombres'] +' '+ doc.data()['apellidos'];
+                  });
+              }
+             else
+                 this.errorLogin = true;
+         });
+        },
         registro: function () {
             firebase.auth().createUserWithEmailAndPassword(this.user, this.pass).then(
                 function (user) {
@@ -175,7 +208,6 @@
       }
   }
 </script>
-
 <style>
   @import './assets/css/app.css';
   @import './assets/css/palette.css';
